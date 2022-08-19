8b8913387cfe3c45690a50743a22cf8efaa9b2c6
neekrasov <neekrasov@gmail.com>
2022-08-19 17:00:30 +0300

    added dynamic component filling

--- src/components/CharacterList/CharacterList.js
+++ src/components/CharacterList/CharacterList.js
@@ -1,51 +1,35 @@
+import { useState, useEffect } from 'react';
+import MarvelAPI from '../../services/api/MarvelAPI';
 import './CharacterList.sass';
-import abyss from '../../img/abyss.jpg';
 
 const CharList = () => {
+
+    const [chars, setChars] = useState([]);
+
+    useEffect(()=>{
+        const api = new MarvelAPI();
+        api.getAllCharacters().then(result => setChars(result))
+    }, [])
+
     return (
         <div className="char__list">
             <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
+                {chars.map((char)=> <CharacterListItem {...char}/>)}
             </ul>
             <button className="button button__main button__long">
                 <div className="inner">load more</div>
             </button>
         </div>
+    )
+}
+
+const CharacterListItem = ({charPreview, charName}) => {
+    let charPreviewStyle = charPreview === "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"? {'object-fit': "contain"} : null
+    return (
+        <li className="char__item">
+            <img style = {charPreviewStyle} src={charPreview} alt="abyss"/>
+            <div className="char__name">{charName}</div>
+        </li>
     )
 }
 
