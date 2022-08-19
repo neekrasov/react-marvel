063035eead57837dd6705f1c5f7407541d3823ea
neekrasov <neekrasov@gmail.com>
2022-08-19 18:00:07 +0300

    fix ux(add loading and errors), added keys

--- src/components/CharacterList/CharacterList.js
+++ src/components/CharacterList/CharacterList.js
@@ -1,20 +1,37 @@
 import { useState, useEffect } from 'react';
 import MarvelAPI from '../../services/api/MarvelAPI';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import LoadSpinner from '../LoadSpinner/LoadSpinner';
 import './CharacterList.sass';
 
 const CharList = () => {
 
     const [chars, setChars] = useState([]);
-
+    const [isLoaded, setLoadedStatus] = useState(false);
+    const [isError, setErrorStatus] = useState(false);
+    
     useEffect(()=>{
         const api = new MarvelAPI();
-        api.getAllCharacters().then(result => setChars(result))
+        api.getAllCharacters().then(result => {
+            setChars(result);
+            setLoadedStatus(true);
+            setErrorStatus(false);
+        }).catch(()=>{
+            setLoadedStatus(false);
+            setErrorStatus(true);
+        })
     }, [])
+
+    const errorMessage = isError ? <ErrorMessage/> : null;
+    const loadSpinner = !isLoaded ? <><li/><LoadSpinner/><li/></> : null;
+    const charListItems = !(loadSpinner || errorMessage) ? chars.map((char)=> <CharacterListItem key={char.id} {...char}/>) : null;
 
     return (
         <div className="char__list">
             <ul className="char__grid">
-                {chars.map((char)=> <CharacterListItem {...char}/>)}
+                {errorMessage}
+                {loadSpinner}
+                {charListItems}
             </ul>
             <button className="button button__main button__long">
                 <div className="inner">load more</div>
@@ -24,7 +41,8 @@ const CharList = () => {
 }
 
 const CharacterListItem = ({charPreview, charName}) => {
-    let charPreviewStyle = charPreview === "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"? {'object-fit': "contain"} : null
+
+    let charPreviewStyle = charPreview === "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"? {objectFit: "contain"} : null
     return (
         <li className="char__item">
             <img style = {charPreviewStyle} src={charPreview} alt="abyss"/>
@@ -32,5 +50,4 @@ const CharacterListItem = ({charPreview, charName}) => {
         </li>
     )
 }
-
 export default CharList;
