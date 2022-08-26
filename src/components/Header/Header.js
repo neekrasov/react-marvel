b0bc7cac5580ee200185f2c0658fea275c12049f
neekrasov <neekrasov@gmail.com>
2022-08-26 21:11:13 +0300

    fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ const AppHeader = () => {
     return (
         <header className="app__header">
             <h1 className="app__title">
-                <Link to="/">
+                <Link to="/react-marvel/">
                     <span>Marvel</span> information portal
                 </Link>
             </h1>
